a14fc1e68023ba49dba2b8c1a951757f6826e288
Nate Walters <newsproutsmedia@gmail.com>
2020-09-18 17:08:34 -0700

    Set up for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 let app = require('express')();
 let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-})
+//log users connecting/disconnecting
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+});
+
+// log messages on the console
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+// send a message to everyone
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
