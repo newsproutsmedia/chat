e500c0d9f89d0852c99812934df106f63739c6d6
Nate Walters <newsproutsmedia@gmail.com>
2020-10-15 12:00:37 -0700

    Added Helmet.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const helmet = require('helmet');
 const socketio = require('socket.io');
 const mailer = require('./utils/mail');
 const formatMessage = require('./utils/messages');
@@ -9,8 +10,13 @@ const { userJoin, getCurrentUser, getUserMessageCount, incrementUserMessageCount
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const server = exports.server = http.createServer(app).listen(PORT, () => console.log(`Server running on port ${PORT}`));;
+const server = exports.server = http.createServer(app).listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});;
 const io = socketio(server);
+
+// use helmet
+app.use(helmet());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
