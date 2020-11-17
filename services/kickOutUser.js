72e11febcee8f4aac83359a27a3bd714e744bdc8
Nate Walters <newsproutsmedia@gmail.com>
2020-11-17 12:02:38 -0800

    (bugfix) Fixed server crashes when kicking out disconnected user

--- services/kickOutUser.js
+++ services/kickOutUser.js
@@ -1,4 +1,5 @@
 const User = require('../services/user');
+const logger = require('../loaders/logger');
 
 module.exports = class KickOutUser {
     constructor({socket, io, id}) {
@@ -10,6 +11,13 @@ module.exports = class KickOutUser {
     kickOutUser() {
         User.setUserTerminated(this.kickedOutUserId);
         const user = User.getCurrentUser(this.kickedOutUserId);
+
+        if(this.io.sockets.sockets[this.kickedOutUserId] === undefined) {
+            logger.info('[service.kickOutUser.kickOutUser', {message: 'User socket is undefined. User already disconnected.'});
+            return User.sendRoomUsers(user.room, {socket: this.socket, io: this.io});
+        }
+
+        logger.info('[service.kickoutUser.kickOutUser()', {message: 'User socket found. Emitting logout and disconnect.'});
         User.emitLogoutUser(user, {socket: this.socket, io: this.io});
         this.io.sockets.sockets[this.kickedOutUserId].disconnect();
     }
